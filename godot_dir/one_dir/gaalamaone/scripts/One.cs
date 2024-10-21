2bc5ad8dc356b08fd13408930db1cc4b1d3076e2
Igor Durbek <igor@igordurbek.com>
2024-10-21 10:16:51 +0200

    frontend: two icons

--- godot_dir/one_dir/gaalamaone/scripts/One.cs
+++ godot_dir/one_dir/gaalamaone/scripts/One.cs
@@ -1,14 +1,42 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class One : Node2D
 {
-    public override void _Ready()
-    {
-        GD.Print("<One>");
-    }
-    
-    public override void _Process(double delta)
-    {
-    }
+	private readonly Dictionary<string, PackedScene?> _packedScenes = [];
+	
+	private const string IconSceneName = "iconScene";
+
+	private bool _shouldInit = true;
+	
+	public override void _Ready()
+	{
+		GD.Print("<One>");
+	}
+	
+	public override void _Process(double delta)
+	{
+		if (_shouldInit)
+		{
+			ShouldInit();
+
+			FirstDraw();
+			
+			_shouldInit = false;
+		}
+	}
+
+	private void ShouldInit()
+	{
+		var iconScene = GD.Load<PackedScene>("res://scenes/icon.tscn");
+		_packedScenes[IconSceneName] = iconScene;
+	}
+
+	private void FirstDraw()
+	{
+		var newIcon = _packedScenes[IconSceneName]!.Instantiate<Node2D>();
+		newIcon.Position = new Vector2(325F, 125F);
+		AddChild(newIcon);
+	}
 }
