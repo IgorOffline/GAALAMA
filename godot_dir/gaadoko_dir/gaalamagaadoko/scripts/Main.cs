a724a93d5f6a921a794c96edda1fe8e7d4246e1d
Igor Durbek <igor@igordurbek.com>
2025-04-14 14:28:07 +0200

    Press BtnSet

--- godot_dir/gaadoko_dir/gaalamagaadoko/scripts/Main.cs
+++ godot_dir/gaadoko_dir/gaalamagaadoko/scripts/Main.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Main : Node
 {
+	private bool _firstRun = true;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,5 +14,28 @@ public partial class Main : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (_firstRun)
+		{
+			var btn = GetNode<Button>("Codeline/BtnSet");
+			if (btn == null)
+			{
+				GD.Print("btn == null");
+			}
+			else
+			{
+				btn.Pressed += () => { 
+					if (btn.Text.Equals(">"))
+					{
+						btn.Text = "<";
+					}
+					else
+					{
+						btn.Text = ">";
+					}
+				};
+			}
+
+			_firstRun = false;
+		}
 	}
 }
