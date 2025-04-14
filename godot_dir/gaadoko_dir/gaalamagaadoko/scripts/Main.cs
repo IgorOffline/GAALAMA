8bc5d7b91c02c20a5ec63321a29745a319276642
Igor Durbek <igor@igordurbek.com>
2025-04-14 23:30:08 +0200

    Gaadoko socket init

--- godot_dir/gaadoko_dir/gaalamagaadoko/scripts/Main.cs
+++ godot_dir/gaadoko_dir/gaalamagaadoko/scripts/Main.cs
@@ -1,5 +1,8 @@
 using Godot;
 using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 public partial class Main : Node
 {
@@ -16,26 +19,78 @@ public partial class Main : Node
 	{
 		if (_firstRun)
 		{
-			var btn = GetNode<Button>("Codeline/BtnSet");
-			if (btn == null)
-			{
-				GD.Print("btn == null");
-			}
-			else
-			{
-				btn.Pressed += () => { 
-					if (btn.Text.Equals(">"))
-					{
-						btn.Text = "<";
-					}
-					else
-					{
-						btn.Text = ">";
-					}
-				};
-			}
-
+			Servero();
+			
 			_firstRun = false;
+		}
+		
+		// if (_firstRun)
+		// {
+		// 	var btn = GetNode<Button>("Codeline/BtnSet");
+		// 	if (btn == null)
+		// 	{
+		// 		GD.Print("btn == null");
+		// 	}
+		// 	else
+		// 	{
+		// 		btn.Pressed += () => { 
+		// 			if (btn.Text.Equals(">"))
+		// 			{
+		// 				btn.Text = "<";
+		// 			}
+		// 			else
+		// 			{
+		// 				btn.Text = ">";
+		// 			}
+		// 		};
+		// 	}
+		//
+		// 	_firstRun = false;
+		// }
+	}
+
+	private async void Servero()
+	{
+		var hostName = Dns.GetHostName();
+		IPHostEntry localhost = await Dns.GetHostEntryAsync(hostName);
+		IPAddress localIpAddress = localhost.AddressList[0];
+		
+		IPEndPoint ipEndPoint = new(localIpAddress, 9_000);
+		
+		using Socket listener = new(
+			ipEndPoint.AddressFamily,
+			SocketType.Stream,
+			ProtocolType.Tcp);
+
+		listener.Bind(ipEndPoint);
+		listener.Listen(100);
+
+		var handler = await listener.AcceptAsync();
+		
+		while (true)
+		{
+			// Receive message.
+			var buffer = new byte[1_024];
+			var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+			var response = Encoding.UTF8.GetString(buffer, 0, received);
+    
+			var eom = "<|EOM|>";
+			if (response.IndexOf(eom) > -1 /* is end of message */)
+			{
+				Console.WriteLine(
+					$"Socket server received message: \"{response.Replace(eom, "")}\"");
+
+				var ackMessage = "<|ACK|>";
+				var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+				await handler.SendAsync(echoBytes, 0);
+				Console.WriteLine(
+					$"Socket server sent acknowledgment: \"{ackMessage}\"");
+
+				break;
+			}
+			// Sample output:
+			//    Socket server received message: "Hi friends 👋!"
+			//    Socket server sent acknowledgment: "<|ACK|>"
 		}
 	}
 }
--- godot_dir/one_dir/GaalamaBusiness/BusinessMain/GaalamaExec.cs
+++ godot_dir/one_dir/GaalamaBusiness/BusinessMain/GaalamaExec.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
 using GaalamaBusiness.BusinessGenerated;
@@ -9,15 +12,60 @@ public class GaalamaExec(ILogger logger)
 {
     public void Execute()
     {
-        var listener = new GaalamaGrammarListenerImpl(logger);
-
-        var lexer = new GaalamaGrammarLexer(new AntlrFileStream("input.txt"));
-        var tokens = new CommonTokenStream(lexer);
-        var parser = new GaalamaGrammarParser(tokens);
-        var tree = parser.gaalamamain();
-        var walker = new ParseTreeWalker();
-        walker.Walk(listener, tree);
+        Asinka();
     }
+
+    private async void Asinka()
+    {
+        var hostName = Dns.GetHostName();
+        IPHostEntry localhost = await Dns.GetHostEntryAsync(hostName);
+        IPAddress localIpAddress = localhost.AddressList[0];
+        
+        IPEndPoint ipEndPoint = new(localIpAddress, 9_000);
+        
+        using Socket client = new(
+            ipEndPoint.AddressFamily, 
+            SocketType.Stream, 
+            ProtocolType.Tcp);
+
+        await client.ConnectAsync(ipEndPoint);
+        while (true)
+        {
+            // Send message.
+            var message = "Hi friends 👋!<|EOM|>";
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            _ = await client.SendAsync(messageBytes, SocketFlags.None);
+            Console.WriteLine($"Socket client sent message: \"{message}\"");
+
+            // Receive ack.
+            var buffer = new byte[1_024];
+            var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+            var response = Encoding.UTF8.GetString(buffer, 0, received);
+            if (response == "<|ACK|>")
+            {
+                Console.WriteLine(
+                    $"Socket client received acknowledgment: \"{response}\"");
+                break;
+            }
+            // Sample output:
+            //     Socket client sent message: "Hi friends 👋!<|EOM|>"
+            //     Socket client received acknowledgment: "<|ACK|>"
+        }
+
+        client.Shutdown(SocketShutdown.Both);
+    }
+    
+    // public void Execute()
+    // {
+    //     var listener = new GaalamaGrammarListenerImpl(logger);
+    //
+    //     var lexer = new GaalamaGrammarLexer(new AntlrFileStream("input.txt"));
+    //     var tokens = new CommonTokenStream(lexer);
+    //     var parser = new GaalamaGrammarParser(tokens);
+    //     var tree = parser.gaalamamain();
+    //     var walker = new ParseTreeWalker();
+    //     walker.Walk(listener, tree);
+    // }
 
     public void Undo()
     {
